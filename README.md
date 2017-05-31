1de1474452139abf0f191dfad24438bf10be910f
Joe Huss <detain@interserver.net>
2017-05-31 17:34:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MyAdmin Whmsonic Licensing Plugin
+
+This plugin adds support for selling Whmsonic licenses to the MyAdmin system
+
+[![Latest Stable Version](https://poser.pugx.org/detain/myadmin-whmsonic-licensing/version)](https://packagist.org/packages/detain/myadmin-whmsonic-licensing)
+[![Total Downloads](https://poser.pugx.org/detain/myadmin-whmsonic-licensing/downloads)](https://packagist.org/packages/detain/myadmin-whmsonic-licensing)
+[![Latest Unstable Version](https://poser.pugx.org/detain/myadmin-whmsonic-licensing/v/unstable)](//packagist.org/packages/detain/myadmin-whmsonic-licensing)
+[![License](https://poser.pugx.org/detain/myadmin-whmsonic-licensing/license)](https://packagist.org/packages/detain/myadmin-whmsonic-licensing)
+[![Monthly Downloads](https://poser.pugx.org/detain/myadmin-whmsonic-licensing/d/monthly)](https://packagist.org/packages/detain/myadmin-whmsonic-licensing)
+[![Daily Downloads](https://poser.pugx.org/detain/myadmin-whmsonic-licensing/d/daily)](https://packagist.org/packages/detain/myadmin-whmsonic-licensing)
+[![Reference Status](https://www.versioneye.com/php/detain:myadmin-whmsonic-licensing/reference_badge.svg?style=flat)](https://www.versioneye.com/php/detain:myadmin-whmsonic-licensing/references)
+[![Build Status](https://travis-ci.org/detain/myadmin-whmsonic-licensing.svg?branch=master)](https://travis-ci.org/detain/myadmin-whmsonic-licensing)
+[![Code Climate](https://codeclimate.com/github/detain/myadmin-whmsonic-licensing/badges/gpa.svg)](https://codeclimate.com/github/detain/myadmin-whmsonic-licensing)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/detain/myadmin-whmsonic-licensing/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/detain/myadmin-whmsonic-licensing/?branch=master)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/dcfdb555bf234afabceb40728959280b)](https://www.codacy.com/app/detain/myadmin-whmsonic-licensing)
---
+++ src/Plugin.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Detain\MyAdminWhmsonic;
+
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class Plugin {
+
+	public function __construct() {
+	}
+
+	public static function Activate(GenericEvent $event) {
+		// will be executed when the licenses.license event is dispatched
+		$license = $event->getSubject();
+		if ($event['category'] == SERVICE_TYPES_WHMSONIC) {
+			myadmin_log('licenses', 'info', 'Whmsonic Activation', __LINE__, __FILE__);
+			function_requirements('activate_whmsonic');
+			activate_whmsonic($license->get_ip(), $event['field1']);
+			$event->stopPropagation();
+		}
+	}
+
+	public static function ChangeIp(GenericEvent $event) {
+		if ($event['category'] == SERVICE_TYPES_WHMSONIC) {
+			$license = $event->getSubject();
+			$settings = get_module_settings('licenses');
+			$whmsonic = new Whmsonic(WHMSONIC_USERNAME, WHMSONIC_PASSWORD);
+			myadmin_log('licenses', 'info', "IP Change - (OLD:".$license->get_ip().") (NEW:{$event['newip']})", __LINE__, __FILE__);
+			$result = $whmsonic->editIp($license->get_ip(), $event['newip']);
+			if (isset($result['faultcode'])) {
+				myadmin_log('licenses', 'error', 'Whmsonic editIp('.$license->get_ip().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__);
+				$event['status'] = 'error';
+				$event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
+			} else {
+				$GLOBALS['tf']->history->add($settings['TABLE'], 'change_ip', $event['newip'], $license->get_ip());
+				$license->set_ip($event['newip'])->save();
+				$event['status'] = 'ok';
+				$event['status_text'] = 'The IP Address has been changed.';
+			}
+			$event->stopPropagation();
+		}
+	}
+
+	public static function Menu(GenericEvent $event) {
+		// will be executed when the licenses.settings event is dispatched
+		$menu = $event->getSubject();
+		$module = 'licenses';
+		if ($GLOBALS['tf']->ima == 'admin') {
+			$menu->add_link($module, 'choice=none.reusable_whmsonic', 'icons/database_warning_48.png', 'ReUsable Whmsonic Licenses');
+			$menu->add_link($module, 'choice=none.whmsonic_list', 'icons/database_warning_48.png', 'Whmsonic Licenses Breakdown');
+			$menu->add_link($module.'api', 'choice=none.whmsonic_licenses_list', 'whm/createacct.gif', 'List all Whmsonic Licenses');
+		}
+	}
+
+	public static function Requirements(GenericEvent $event) {
+		// will be executed when the licenses.loader event is dispatched
+		$loader = $event->getSubject();
+		$loader->add_requirement('class.Whmsonic', '/../vendor/detain/myadmin-whmsonic-licensing/src/Whmsonic.php');
+		$loader->add_requirement('deactivate_whmsonic', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
+	}
+
+	public static function Settings(GenericEvent $event) {
+		// will be executed when the licenses.settings event is dispatched
+		$settings = $event->getSubject();
+		$settings->add_text_setting('apisettings', 'whmsonic_api_username', 'Whmsonic API Username:', 'Whmsonic API Username', $settings->get_setting('WHMSONIC_API_USERNAME'));
+		$settings->add_text_setting('apisettings', 'whmsonic_api_key', 'Whmsonic API Key:', 'Whmsonic API Key', $settings->get_setting('WHMSONIC_API_KEY'));
+		$settings->add_dropdown_setting('stock', 'outofstock_licenses_whmsonic', 'Out Of Stock Whmsonic Licenses', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LICENSES_WHMSONIC'), array('0', '1'), array('No', 'Yes', ));
+	}
+
+}
