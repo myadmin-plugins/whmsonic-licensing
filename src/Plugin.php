745d32d3b3b69679d70ae39acef202432b38a335
Joe Huss <detain@interserver.net>
2017-06-07 08:26:45 -0400

    updates to settings

--- src/Plugin.php
+++ src/Plugin.php
@@ -67,9 +67,9 @@ class Plugin {
 	public static function Settings(GenericEvent $event) {
 		// will be executed when the licenses.settings event is dispatched
 		$settings = $event->getSubject();
-		$settings->add_text_setting('licenses', 'apisettings', 'whmsonic_username', 'Whmsonic Username:', 'Whmsonic Username', $settings->get_setting('WHMSONIC_USERNAME'));
-		$settings->add_text_setting('licenses', 'apisettings', 'whmsonic_password', 'Whmsonic Password:', 'Whmsonic Password', $settings->get_setting('WHMSONIC_PASSWORD'));
-		$settings->add_dropdown_setting('licenses', 'stock', 'outofstock_licenses_whmsonic', 'Out Of Stock Whmsonic Licenses', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LICENSES_WHMSONIC'), array('0', '1'), array('No', 'Yes', ));
+		$settings->add_text_setting('licenses', 'WHMSonic', 'whmsonic_username', 'Whmsonic Username:', 'Whmsonic Username', $settings->get_setting('WHMSONIC_USERNAME'));
+		$settings->add_text_setting('licenses', 'WHMSonic', 'whmsonic_password', 'Whmsonic Password:', 'Whmsonic Password', $settings->get_setting('WHMSONIC_PASSWORD'));
+		$settings->add_dropdown_setting('licenses', 'WHMSonic', 'outofstock_licenses_whmsonic', 'Out Of Stock Whmsonic Licenses', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LICENSES_WHMSONIC'), array('0', '1'), array('No', 'Yes', ));
 	}
 
 }
