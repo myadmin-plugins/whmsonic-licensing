b99419dc06487375f8a2ae9fb5e64f07b9a9a50a
Joe Huss <detain@interserver.net>
2017-07-20 01:36:19 -0400

    removing unneeded paranthesis

--- bin/whmsonic_licenses.php
+++ bin/whmsonic_licenses.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 
-require_once(__DIR__.'/../../../../include/functions.inc.php');
+require_once __DIR__.'/../../../../include/functions.inc.php';
 
 print_r(whmsonic_list());
 
