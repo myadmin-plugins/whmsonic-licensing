f9f200d23edc78d373f3ec60e803a3660b75980f
Joe Huss <detain@interserver.net>
2022-07-28 17:06:31 -0400

    formatting update

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,112 +11,112 @@ use Symfony\Component\EventDispatcher\GenericEvent;
  */
 class Plugin
 {
-	public static $name = 'WHMSonic Licensing';
-	public static $description = 'Allows selling of WHMSonic License Types. WHMSonic is the best WHM/cPanel plugin(shoutcast control panel), intended on making your life easier. You can install it with a single SSH command in seconds and it allows you to offer shoutcast, icecast, streaming media hosting, AutoDJ, radio reseller from your Dedicated or VPS server. It has powerful control panel for your cpanel clients and resellers. Clients can fully control their radios easily. It has everything you need to start and manage your shoutcast radio service hosting.  More info at https://www.whmsonic.com/';
-	public static $help = '';
-	public static $module = 'licenses';
-	public static $type = 'service';
+    public static $name = 'WHMSonic Licensing';
+    public static $description = 'Allows selling of WHMSonic License Types. WHMSonic is the best WHM/cPanel plugin(shoutcast control panel), intended on making your life easier. You can install it with a single SSH command in seconds and it allows you to offer shoutcast, icecast, streaming media hosting, AutoDJ, radio reseller from your Dedicated or VPS server. It has powerful control panel for your cpanel clients and resellers. Clients can fully control their radios easily. It has everything you need to start and manage your shoutcast radio service hosting.  More info at https://www.whmsonic.com/';
+    public static $help = '';
+    public static $module = 'licenses';
+    public static $type = 'service';
 
-	/**
-	 * Plugin constructor.
-	 */
-	public function __construct()
-	{
-	}
+    /**
+     * Plugin constructor.
+     */
+    public function __construct()
+    {
+    }
 
-	/**
-	 * @return array
-	 */
-	public static function getHooks()
-	{
-		return [
-			'function.requirements' => [__CLASS__, 'getRequirements'],
-			self::$module.'.settings' => [__CLASS__, 'getSettings'],
-			self::$module.'.activate' => [__CLASS__, 'getActivate'],
-			self::$module.'.reactivate' => [__CLASS__, 'getActivate']
-		];
-	}
+    /**
+     * @return array
+     */
+    public static function getHooks()
+    {
+        return [
+            'function.requirements' => [__CLASS__, 'getRequirements'],
+            self::$module.'.settings' => [__CLASS__, 'getSettings'],
+            self::$module.'.activate' => [__CLASS__, 'getActivate'],
+            self::$module.'.reactivate' => [__CLASS__, 'getActivate']
+        ];
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getActivate(GenericEvent $event)
-	{
-		$serviceClass = $event->getSubject();
-		if ($event['category'] == get_service_define('WHMSONIC')) {
-			myadmin_log(self::$module, 'info', 'WHMSonic Activation', __LINE__, __FILE__, self::$module, $serviceClass->getId());
-			function_requirements('activate_whmsonic');
-			activate_whmsonic($serviceClass->getIp(), $event['field1'], $serviceClass->getId(), $event['email'], $event['email']);
-			$event->stopPropagation();
-		}
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getActivate(GenericEvent $event)
+    {
+        $serviceClass = $event->getSubject();
+        if ($event['category'] == get_service_define('WHMSONIC')) {
+            myadmin_log(self::$module, 'info', 'WHMSonic Activation', __LINE__, __FILE__, self::$module, $serviceClass->getId());
+            function_requirements('activate_whmsonic');
+            activate_whmsonic($serviceClass->getIp(), $event['field1'], $serviceClass->getId(), $event['email'], $event['email']);
+            $event->stopPropagation();
+        }
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getChangeIp(GenericEvent $event)
-	{
-		if ($event['category'] == get_service_define('WHMSONIC')) {
-			$serviceClass = $event->getSubject();
-			$settings = get_module_settings(self::$module);
-			$whmsonic = new Whmsonic(WHMSONIC_USERNAME, WHMSONIC_PASSWORD);
-			myadmin_log(self::$module, 'info', 'IP Change - (OLD:' .$serviceClass->getIp().") (NEW:{$event['newip']})", __LINE__, __FILE__, self::$module, $serviceClass->getId());
-			$result = $whmsonic->editIp($serviceClass->getIp(), $event['newip']);
-			if (isset($result['faultcode'])) {
-				myadmin_log(self::$module, 'error', 'WHMSonic editIp('.$serviceClass->getIp().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__, self::$module, $serviceClass->getId());
-				$event['status'] = 'error';
-				$event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
-			} else {
-				$GLOBALS['tf']->history->add($settings['TABLE'], 'change_ip', $event['newip'], $serviceClass->getId(), $serviceClass->getCustid());
-				$serviceClass->set_ip($event['newip'])->save();
-				$event['status'] = 'ok';
-				$event['status_text'] = 'The IP Address has been changed.';
-			}
-			$event->stopPropagation();
-		}
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getChangeIp(GenericEvent $event)
+    {
+        if ($event['category'] == get_service_define('WHMSONIC')) {
+            $serviceClass = $event->getSubject();
+            $settings = get_module_settings(self::$module);
+            $whmsonic = new Whmsonic(WHMSONIC_USERNAME, WHMSONIC_PASSWORD);
+            myadmin_log(self::$module, 'info', 'IP Change - (OLD:' .$serviceClass->getIp().") (NEW:{$event['newip']})", __LINE__, __FILE__, self::$module, $serviceClass->getId());
+            $result = $whmsonic->editIp($serviceClass->getIp(), $event['newip']);
+            if (isset($result['faultcode'])) {
+                myadmin_log(self::$module, 'error', 'WHMSonic editIp('.$serviceClass->getIp().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__, self::$module, $serviceClass->getId());
+                $event['status'] = 'error';
+                $event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
+            } else {
+                $GLOBALS['tf']->history->add($settings['TABLE'], 'change_ip', $event['newip'], $serviceClass->getId(), $serviceClass->getCustid());
+                $serviceClass->set_ip($event['newip'])->save();
+                $event['status'] = 'ok';
+                $event['status_text'] = 'The IP Address has been changed.';
+            }
+            $event->stopPropagation();
+        }
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getMenu(GenericEvent $event)
-	{
-		$menu = $event->getSubject();
-		if ($GLOBALS['tf']->ima == 'admin') {
-			$menu->add_link(self::$module, 'choice=none.reusable_whmsonic', '/images/myadmin/to-do.png', _('ReUsable WHMSonic Licenses'));
-			$menu->add_link(self::$module, 'choice=none.whmsonic_list', '/images/myadmin/to-do.png', _('WHMSonic Licenses Breakdown'));
-			$menu->add_link(self::$module.'api', 'choice=none.whmsonic_licenses_list', '/images/whm/createacct.gif', _('List all WHMSonic Licenses'));
-		}
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getMenu(GenericEvent $event)
+    {
+        $menu = $event->getSubject();
+        if ($GLOBALS['tf']->ima == 'admin') {
+            $menu->add_link(self::$module, 'choice=none.reusable_whmsonic', '/images/myadmin/to-do.png', _('ReUsable WHMSonic Licenses'));
+            $menu->add_link(self::$module, 'choice=none.whmsonic_list', '/images/myadmin/to-do.png', _('WHMSonic Licenses Breakdown'));
+            $menu->add_link(self::$module.'api', 'choice=none.whmsonic_licenses_list', '/images/whm/createacct.gif', _('List all WHMSonic Licenses'));
+        }
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getRequirements(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Plugins\Loader $this->loader
-		 */
-		$loader = $event->getSubject();
-		$loader->add_requirement('activate_whmsonic', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
-		$loader->add_page_requirement('whmsonic_terminate', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
-		$loader->add_page_requirement('whmsonic_suspend', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
-		$loader->add_page_requirement('whmsonic_unsuspend', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
-		$loader->add_page_requirement('whmsonic_list', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
-		$loader->add_page_requirement('whmsonic_verify', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getRequirements(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Plugins\Loader $this->loader
+         */
+        $loader = $event->getSubject();
+        $loader->add_requirement('activate_whmsonic', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
+        $loader->add_page_requirement('whmsonic_terminate', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
+        $loader->add_page_requirement('whmsonic_suspend', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
+        $loader->add_page_requirement('whmsonic_unsuspend', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
+        $loader->add_page_requirement('whmsonic_list', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
+        $loader->add_page_requirement('whmsonic_verify', '/../vendor/detain/myadmin-whmsonic-licensing/src/whmsonic.inc.php');
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getSettings(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Settings $settings
-		 **/
-		$settings = $event->getSubject();
-		$settings->add_text_setting(self::$module, _('WHMSonic'), 'whmsonic_username', _('WHMSonic Username'), _('WHMSonic Username'), $settings->get_setting('WHMSONIC_USERNAME'));
-		$settings->add_text_setting(self::$module, _('WHMSonic'), 'whmsonic_password', _('WHMSonic Password'), _('WHMSonic Password'), $settings->get_setting('WHMSONIC_PASSWORD'));
-		$settings->add_dropdown_setting(self::$module, _('WHMSonic'), 'outofstock_licenses_whmsonic', _('Out Of Stock WHMSonic Licenses'), _('Enable/Disable Sales Of This Type'), $settings->get_setting('OUTOFSTOCK_LICENSES_WHMSONIC'), ['0', '1'], ['No', 'Yes']);
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getSettings(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Settings $settings
+         **/
+        $settings = $event->getSubject();
+        $settings->add_text_setting(self::$module, _('WHMSonic'), 'whmsonic_username', _('WHMSonic Username'), _('WHMSonic Username'), $settings->get_setting('WHMSONIC_USERNAME'));
+        $settings->add_text_setting(self::$module, _('WHMSonic'), 'whmsonic_password', _('WHMSonic Password'), _('WHMSonic Password'), $settings->get_setting('WHMSONIC_PASSWORD'));
+        $settings->add_dropdown_setting(self::$module, _('WHMSonic'), 'outofstock_licenses_whmsonic', _('Out Of Stock WHMSonic Licenses'), _('Enable/Disable Sales Of This Type'), $settings->get_setting('OUTOFSTOCK_LICENSES_WHMSONIC'), ['0', '1'], ['No', 'Yes']);
+    }
 }
--- src/whmsonic.inc.php
+++ src/whmsonic.inc.php
@@ -25,41 +25,41 @@
  */
 function activate_whmsonic($licenseip, $license, $orderid, $clientName, $clientEmail)
 {
-	/*
-	* // License Type & Client Details
-	* $license = ""; // License Type    Example; $license = "$ipAddress_from_my_website_form";
-	* $licenseip = ""; // LicenseIP, mainserverip of the dedicated server or VPS.
-	* $clientEmail = ""; // Must be filled
-	* $clientName = ""; // Must be filled
-	*/
-	$resellerusername = WHMSONIC_USERNAME;
-	$resellerpassword = WHMSONIC_PASSWORD;
-	//    $orderid = ""; // The orderID or saleID, this is very useful for manual updates etc.. It will be stored on our side in the database as well.
+    /*
+    * // License Type & Client Details
+    * $license = ""; // License Type    Example; $license = "$ipAddress_from_my_website_form";
+    * $licenseip = ""; // LicenseIP, mainserverip of the dedicated server or VPS.
+    * $clientEmail = ""; // Must be filled
+    * $clientName = ""; // Must be filled
+    */
+    $resellerusername = WHMSONIC_USERNAME;
+    $resellerpassword = WHMSONIC_PASSWORD;
+    //    $orderid = ""; // The orderID or saleID, this is very useful for manual updates etc.. It will be stored on our side in the database as well.
 
-	// Code to perform action goes here...
+    // Code to perform action goes here...
 
-	$url = 'http://www.whmsonic.com/api/action.php?';
-	$fieldstring = "cmd=create&username=$resellerusername&upass=$resellerpassword&license=$license&licenseip=$licenseip&client_email=$clientEmail&client_name=$clientName&orderid=$orderid&spamprotection=654a65z4a9AAQZloqe";
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 59);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	$retval = curl_exec($ch);
-	if (curl_errno($ch)) {
-		$retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
-	}
-	curl_close($ch);
+    $url = 'http://www.whmsonic.com/api/action.php?';
+    $fieldstring = "cmd=create&username=$resellerusername&upass=$resellerpassword&license=$license&licenseip=$licenseip&client_email=$clientEmail&client_name=$clientName&orderid=$orderid&spamprotection=654a65z4a9AAQZloqe";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 59);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $retval = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
+    }
+    curl_close($ch);
 
-	if ($retval == 'Complete') {
-		$result = 'success';
-	} else {
-		$result = "<br>$retval";
-	}
-	return $result;
+    if ($retval == 'Complete') {
+        $result = 'success';
+    } else {
+        $result = "<br>$retval";
+    }
+    return $result;
 }
 
 /**
@@ -71,35 +71,35 @@ function activate_whmsonic($licenseip, $license, $orderid, $clientName, $clientE
  */
 function whmsonic_terminate($licenseip)
 {
-	// License Type & Client Details
-	//    $licenseip = ""; // LicenseIP, mainserverip of the dedicated server or VPS.
-	$resellerusername = WHMSONIC_USERNAME;
-	$resellerpassword = WHMSONIC_PASSWORD;
+    // License Type & Client Details
+    //    $licenseip = ""; // LicenseIP, mainserverip of the dedicated server or VPS.
+    $resellerusername = WHMSONIC_USERNAME;
+    $resellerpassword = WHMSONIC_PASSWORD;
 
-	// Code to perform action goes here...
+    // Code to perform action goes here...
 
-	$url = 'http://www.whmsonic.com/api/action.php?';
-	$fieldstring = "cmd=terminate&username=$resellerusername&upass=$resellerpassword&licenseip=$licenseip&spamprotection=654a65z4a9AAQZloqe";
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 59);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	$retval = curl_exec($ch);
-	if (curl_errno($ch)) {
-		$retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
-	}
-	curl_close($ch);
+    $url = 'http://www.whmsonic.com/api/action.php?';
+    $fieldstring = "cmd=terminate&username=$resellerusername&upass=$resellerpassword&licenseip=$licenseip&spamprotection=654a65z4a9AAQZloqe";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 59);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $retval = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
+    }
+    curl_close($ch);
 
-	if ($retval == 'Complete') {
-		$result = 'success';
-	} else {
-		$result = "<br>$retval";
-	}
-	return $result;
+    if ($retval == 'Complete') {
+        $result = 'success';
+    } else {
+        $result = "<br>$retval";
+    }
+    return $result;
 }
 
 /**
@@ -111,34 +111,34 @@ function whmsonic_terminate($licenseip)
  */
 function whmsonic_suspend($licenseip)
 {
-	// License Type & Client Details
-	//    $licenseip = ""; // LicenseIP, mainserverip of the dedicated server or VPS.
-	$resellerusername = WHMSONIC_USERNAME;
-	$resellerpassword = WHMSONIC_PASSWORD;
-	// Code to perform action goes here...
+    // License Type & Client Details
+    //    $licenseip = ""; // LicenseIP, mainserverip of the dedicated server or VPS.
+    $resellerusername = WHMSONIC_USERNAME;
+    $resellerpassword = WHMSONIC_PASSWORD;
+    // Code to perform action goes here...
 
-	$url = 'http://www.whmsonic.com/api/action.php?';
-	$fieldstring = "cmd=suspend&username=$resellerusername&upass=$resellerpassword&licenseip=$licenseip&spamprotection=654a65z4a9AAQZloqe";
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 59);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	$retval = curl_exec($ch);
-	if (curl_errno($ch)) {
-		$retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
-	}
-	curl_close($ch);
+    $url = 'http://www.whmsonic.com/api/action.php?';
+    $fieldstring = "cmd=suspend&username=$resellerusername&upass=$resellerpassword&licenseip=$licenseip&spamprotection=654a65z4a9AAQZloqe";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 59);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $retval = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
+    }
+    curl_close($ch);
 
-	if ($retval == 'Complete') {
-		$result = 'success';
-	} else {
-		$result = "<br>$retval";
-	}
-	return $result;
+    if ($retval == 'Complete') {
+        $result = 'success';
+    } else {
+        $result = "<br>$retval";
+    }
+    return $result;
 }
 
 /**
@@ -151,35 +151,35 @@ function whmsonic_suspend($licenseip)
 function whmsonic_unsuspend($licenseip)
 {
 
-	// License Type & Client Details
-	//    $licenseip = ""; // LicenseIP, mainserverip of the dedicated server or VPS.
-	$resellerusername = WHMSONIC_USERNAME;
-	$resellerpassword = WHMSONIC_PASSWORD;
+    // License Type & Client Details
+    //    $licenseip = ""; // LicenseIP, mainserverip of the dedicated server or VPS.
+    $resellerusername = WHMSONIC_USERNAME;
+    $resellerpassword = WHMSONIC_PASSWORD;
 
-	// Code to perform action goes here...
+    // Code to perform action goes here...
 
-	$url = 'http://www.whmsonic.com/api/action.php?';
-	$fieldstring = "cmd=unsuspend&username=$resellerusername&upass=$resellerpassword&licenseip=$licenseip&spamprotection=654a65z4a9AAQZloqe";
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 59);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	$retval = curl_exec($ch);
-	if (curl_errno($ch)) {
-		$retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
-	}
-	curl_close($ch);
+    $url = 'http://www.whmsonic.com/api/action.php?';
+    $fieldstring = "cmd=unsuspend&username=$resellerusername&upass=$resellerpassword&licenseip=$licenseip&spamprotection=654a65z4a9AAQZloqe";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 59);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $retval = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
+    }
+    curl_close($ch);
 
-	if ($retval == 'Complete') {
-		$result = 'success';
-	} else {
-		$result = "<br>$retval";
-	}
-	return $result;
+    if ($retval == 'Complete') {
+        $result = 'success';
+    } else {
+        $result = "<br>$retval";
+    }
+    return $result;
 }
 
 /**
@@ -197,33 +197,33 @@ function whmsonic_unsuspend($licenseip)
  */
 function whmsonic_list($type = 4)
 {
-	// SECURITY WARNING: You may not run this script directly under your website,
-	// you may use it under the password protected folder, encode the script or change the file name in order to protect your clients serverips.
+    // SECURITY WARNING: You may not run this script directly under your website,
+    // you may use it under the password protected folder, encode the script or change the file name in order to protect your clients serverips.
 
-	$resellerusername = WHMSONIC_USERNAME; // Your WHMSonic reseller username
-	$resellerpassword = WHMSONIC_PASSWORD; // Your WHMSonic reseller password
+    $resellerusername = WHMSONIC_USERNAME; // Your WHMSonic reseller username
+    $resellerpassword = WHMSONIC_PASSWORD; // Your WHMSonic reseller password
 
-	$url = 'http://www.whmsonic.com/api/list.php?';
-	$fieldstring = "cmd=list&username=$resellerusername&upass=$resellerpassword&type=$type&spamprotection=654a65z4a9AAQZloqe";
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 59);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	$retval = curl_exec($ch);
-	if (curl_errno($ch)) {
-		$retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
-	}
-	curl_close($ch);
-	if ($retval == 'You dont have a registered  license') {
-		$licenses = [];
-	} else {
-		$licenses = implode("\n", $retval);
-	}
-	return $licenses;
+    $url = 'http://www.whmsonic.com/api/list.php?';
+    $fieldstring = "cmd=list&username=$resellerusername&upass=$resellerpassword&type=$type&spamprotection=654a65z4a9AAQZloqe";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 59);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $retval = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
+    }
+    curl_close($ch);
+    if ($retval == 'You dont have a registered  license') {
+        $licenses = [];
+    } else {
+        $licenses = implode("\n", $retval);
+    }
+    return $licenses;
 }
 
 /**
@@ -237,29 +237,29 @@ function whmsonic_list($type = 4)
  */
 function whmsonic_verify($clientserverIP)
 {
-	// Request this variable $clientserverIP from the client to check if the license is valid or not.
-	// This must be a mainIP of the server, let the client submit their licensed mainserverIP via form from your website
-	// and send the form data to this page to verify.
+    // Request this variable $clientserverIP from the client to check if the license is valid or not.
+    // This must be a mainIP of the server, let the client submit their licensed mainserverIP via form from your website
+    // and send the form data to this page to verify.
 
-	$url = 'http://www.whmsonic.com/verify2.php'; // Manual Test: http://www.whmsonic.com/verify2.php?ip=67.228.53.90 will return yes. Change the ip to no.
-	$fieldstring = "ip=$clientserverIP";
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 59);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	$retval = curl_exec($ch);
-	if (curl_errno($ch)) {
-		$retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
-	}
-	curl_close($ch);
+    $url = 'http://www.whmsonic.com/verify2.php'; // Manual Test: http://www.whmsonic.com/verify2.php?ip=67.228.53.90 will return yes. Change the ip to no.
+    $fieldstring = "ip=$clientserverIP";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldstring);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 59);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $retval = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $retval = 'CURL Error: '.curl_errno($ch).' - '.curl_error($ch);
+    }
+    curl_close($ch);
 
-	if ($retval == 'yes') {
-		echo 'ok'; // Do the things here you need. The client has a valid license.
-	} else {
-		echo 'failed'; // Tell the client that they don't have a valid license.
-	}
+    if ($retval == 'yes') {
+        echo 'ok'; // Do the things here you need. The client has a valid license.
+    } else {
+        echo 'failed'; // Tell the client that they don't have a valid license.
+    }
 }
